609de0d562429a5333e76a9c5684a1a3efa6498a
kwakuNti <159504184+kwakuNti@users.noreply.github.com>
2024-10-11 18:30:06 +0000

    Refactor login and homepage CSS code

--- templates/sign-up.php
+++ templates/sign-up.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
+	<title>Sign Up</title>
 	<link rel="stylesheet" type="text/css" href="../public/css/login.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
@@ -18,30 +18,67 @@
 			<img src="../assets/images/login-removebg-preview.png">
 		</div>
 		<div class="login-content">
-			<form action="index.html">
+			<form action="signup_process.html">
 				<img src="../assets/images/avatar-svgrepo-com.svg">
-				<h2 class="title">Welcome</h2>
+				<h2 class="title">Sign Up</h2>
+
+				<!-- First Name -->
            		<div class="input-div one">
            		   <div class="i">
            		   		<i class="fas fa-user"></i>
            		   </div>
            		   <div class="div">
-           		   		<h5>Email</h5>
-           		   		<input type="text" class="input">
+           		   		<h5>First Name</h5>
+           		   		<input type="text" class="input" required>
            		   </div>
            		</div>
+
+				<!-- Last Name -->
+				<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Last Name</h5>
+           		   		<input type="text" class="input" required>
+           		   </div>
+           		</div>
+
+				<!-- Email -->
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-envelope"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Email</h5>
+           		   		<input type="email" class="input" required>
+           		   </div>
+           		</div>
+
+				<!-- Password -->
            		<div class="input-div pass">
            		   <div class="i"> 
            		    	<i class="fas fa-lock"></i>
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
+           		    	<input type="password" class="input" required>
             	   </div>
             	</div>
-            	<a href="#">Forgot Password?</a>
-                <a href="sign-up">Sign up</a>
-            	<input type="submit" class="btn" value="Login">
+
+				<!-- Confirm Password -->
+				<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Confirm Password</h5>
+           		    	<input type="password" class="input" required>
+            	   </div>
+            	</div>
+				<a href="login">Already have account? Login</a>
+
+				<input type="submit" class="btn" value="Sign Up">
             </form>
         </div>
     </div>
