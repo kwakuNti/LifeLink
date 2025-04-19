e22799e48d0b2259639365ed09d7dd8f79650051
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2025-04-19 23:05:46 +0000

    Refactor invoke-chaincode.sh and query-chaincode.sh: update CORE_PEER_MSPCONFIGPATH to a consistent directory

--- blockchain/invoke-chaincode.sh
+++ blockchain/invoke-chaincode.sh
@@ -15,7 +15,7 @@ export FABRIC_CFG_PATH="/opt/fabric-config"
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE="/home/ubuntu/go/src/github.com/kwakuNti/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
-export CORE_PEER_MSPCONFIGPATH="/home/ubuntu/go/src/github.com/kwakuNti/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
+export CORE_PEER_MSPCONFIGPATH="/opt/fabric-identities/org1/msp"
 export CORE_PEER_ADDRESS="localhost:7051"
 
 # Certificate paths
