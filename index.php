058c1e196b74a4742f3bffbd5981db0d7018e49d
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2025-04-21 14:47:27 +0000

    Fix redirect URL in index.php to remove trailing slash

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
   // at the very top of /opt/lampp/htdocs/LifeLink/index.php
-    header("Location: /templates/homepage/");
+    header("Location: /templates/homepage");
   exit();
 // include '../config/connection.php';
