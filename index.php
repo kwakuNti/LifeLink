45738806eee8ca2259028114881cda3c3c160ecc
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2025-04-21 14:45:33 +0000

    Refactor database connection handling and remove session check in index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
   // at the very top of /opt/lampp/htdocs/LifeLink/index.php
-  session_start();
-  if (!isset($_SESSION['user_id'])) {
     header("Location: /templates/homepage/");
-    exit();
-  }
+  exit();
+// include '../config/connection.php';
